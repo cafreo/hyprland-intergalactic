b12de2601f66f19bc49d9f58d80dbd9dc61de568
cafreo <taehyun_uses_tools.se9ou@slmail.me>
2023-06-02 15:00:08 +0200

    screenshot menu
    - added a rofi menu to pick between 4 screenshot options: area copy, area save, window save, screen save
    - updated hyprland.conf workspace options since wsbind is deprecated
    - made playerctl notification more responsive by using notification IDs instead of tags
    - deleted old screenshot script

--- scripts/dunst/dunst_playerctl.sh
+++ scripts/dunst/dunst_playerctl.sh
@@ -9,9 +9,15 @@ csCoverImg=$(convert "$csCoverUrl" -flatten -thumbnail 80x80 $csCover)
 
 convert "$csCoverUrl" -flatten -thumbnail 80x80 $csCover
 
+wait
+
+dunstify -C 4533
+
 if [ $(playerctl status) == "Playing" ]
 then
-    dunstify -t 3000 -a "playerctl" -b "  Now Playing" "<b>$csTitle</b>\n$csArtist" -u low -i "$csCover" -h string:x-dunst-stack-tag:$msgTag
+    dunstify -t 3000 -a "playerctl" -b "  Now Playing" "<b>$csTitle</b>\n$csArtist" -u low -i "$csCover" -r 4533
 else
-    dunstify -t 3000 -a "playerctl" -b "  Paused" "<b>$csTitle</b>\n$csArtist" -u low -i "$csCover" -h string:x-dunst-stack-tag:$msgTag
+    dunstify -t 3000 -a "playerctl" -b "  Paused" "<b>$csTitle</b>\n$csArtist" -u low -i "$csCover" -r 4533
 fi
+
+#-h string:x-dunst-stack-tag:$msgTag
---
+++ scripts/rofi/rofi-screenshot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+areaCopy='areaCopy\0icon\x1f/home/cafreo/Scripts/img/ssAreaCopy.svg'
+areaSave='areaSave\0icon\x1f/home/cafreo/Scripts/img/ssAreaSave.svg'
+screenSave='screenSave\0icon\x1f/home/cafreo/Scripts/img/ssScreen.svg'
+windowSave='windowSave\0icon\x1f/home/cafreo/Scripts/img/ssWindow.svg'
+
+
+chosen=$(printf "$areaCopy;$areaSave;$screenSave;$windowSave" | rofi -dmenu -p 'Screenshot' -sep ';' -show-icons -theme /home/cafreo/.config/rofi/config-buttons.rasi )
+
+case "$chosen" in
+    "areaCopy")
+        grimblast --notify copy area ;;
+    "areaSave")
+        grimblast --notify copysave area /home/cafreo/Pictures/Screenshots/Screenshot_$(date '+%d-%m-%Y_%H-%M-%S').png ;;
+    "screenSave")
+        grimblast --notify copysave output /home/cafreo/Pictures/Screenshots/Screenshot_$(date '+%d-%m-%Y_%H-%M-%S').png ;;
+    "windowSave")
+        sleep 3 && grimblast --notify copysave active /home/cafreo/Pictures/Screenshots/Screenshot_$(date '+%d-%m-%Y_%H-%M-%S').png;;
+        
+        *) exit 1 ;;
+esac
