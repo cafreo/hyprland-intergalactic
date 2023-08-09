fa28191e6dec7aee12a4fdd3094a68bac51f8bda
cafreo <taehyun_uses_tools.se9ou@slmail.me>
2023-08-09 19:34:52 +0200

    gammastep dunst notifications
    show notification when status of gammastep is switched

--- dotconfig/waybar/scripts/gammastep.sh
+++ dotconfig/waybar/scripts/gammastep.sh
@@ -4,17 +4,16 @@ if [[ $1 = "toggle" ]]; then
 	if pgrep -x "gammastep" > /dev/null; then
 		kill $(pgrep -x gammastep)
 		echo "Nightlight is off"
- 
+		dunstify -t 3000 -a "gammastep" -b "Gammastep" "<b>Nightlight is off</b>" -u low -i ~/Scripts/img/sun.svg -r 7113
 	else
 		gammastep &
-
+		dunstify -t 3000 -a "gammastep" -b "Gammastep" "<b>Nightlight timer is on</b>" -u low -i ~/Scripts/img/moon.svg -r 7113
 	fi
 fi
 
 if pgrep -x "gammastep" > /dev/null; then
 	echo ""
 	echo "Nightlight timer is on"
-
 else
 	echo ""
 	echo "Nightlight is off"
