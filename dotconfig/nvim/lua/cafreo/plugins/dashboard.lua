b4234c5d3cd4022ace1039725138ccb842fa9727
cafreo <taehyun_uses_tools.se9ou@slmail.me>
2023-12-28 03:12:20 +0100

    added nvim dashboard

--- dotconfig/nvim/lua/cafreo/plugins/dashboard.lua
+++ dotconfig/nvim/lua/cafreo/plugins/dashboard.lua
@@ -1,22 +1,79 @@
 return{
 	'nvimdev/dashboard-nvim',
+	dependencies = { {'nvim-tree/nvim-web-devicons'}},
   	event = 'VimEnter',
- 	dependencies = { {'nvim-tree/nvim-web-devicons'}},
+ 
 	config = function()
-
-    require('dashboard').setup {
-    	theme = 'doom',
+    require('dashboard').setup ({
+    	theme = "doom",
 
     	config = {
     		header = {
-				"                                    ██              ",
-				" ▄▄ ▄▄▄     ▄▄▄▄    ▄▄▄   ▄▄▄▄ ▄▄▄ ▄▄▄  ▄▄ ▄▄ ▄▄    ",
-				"  ██  ██  ▄█▄▄▄██ ▄█  ▀█▄  ▀█▄  █   ██   ██ ██ ██   ",
-				"  ██  ██  ██      ██   ██   ▀█▄█    ██   ██ ██ ██   ",
-				" ▄██▄ ██▄  ▀█▄▄▄▀  ▀█▄▄█▀    ▀█    ▄██▄ ▄██ ██ ██▄  ",
-    		},
-    	},
-    }
+[[                                           ]],
+[[                                           ]],
+[[                             _             ]],
+[[                            (_)            ]],
+[[  _ __    ___   ___  __   __ _  _ __ ___   ]],
+[[ | '_ \  / _ \ / _ \ \ \ / /| || '_ ` _ \  ]],
+[[ | | | ||  __/| (_) | \ V / | || | | | | | ]],
+[[ |_| |_| \___| \___/   \_/  |_||_| |_| |_| ]],
+[[                                           ]],
+[[                                           ]],
+            },
+    		--for doom theme
+    		
+    		center = {
+			{
+                icon = "󰻭  ",
+                desc = "new file",
+                key = "e",
+                key_format = " | %s",
+				action = "enew",
+			},
+    		{
+                icon = "󰮗  ",
+                desc = "find file",
+                key = "f",
+                key_format = " | %s",
+                action = "Telescope find_files find_command=rg,--hidden,--files",
+            },
+			{
+                icon = "󰷏  ",
+                desc = "open",
+                key = "o",
+                key_format = " | %s",
+                action = "Telescope file_browser",
+            },
+
+            
+			{
+    			icon = "󱑎  ",
+    			desc = "recent files",
+  				key = "r",
+  				key_format = " | %s",
+    			action = "Telescope oldfiles",
+			},
+			{
+                icon = "󰗼  ",
+                desc = "quit",
+                key = "q",
+                key_format = " | %s",
+            	action = "q!",
+            },
+	    },
+
+		--for hyper theme
+		--[[
+  		packages = { enable = true },
+  		project = { enable = false, limit = 8, icon = 'your icon', label = '', action = 'Telescope find_files cwd=' },
+  		mru = { limit = 6, icon = '󱑎 ', label =  'recent files', cwd_only = false },
+  		--]]
+	    footer = {
+                    "",
+                    string.format("%d plugins loaded", require("lazy").stats().count),
+                },	
+	},
+    })
     
-  	end,
+end,
 }
