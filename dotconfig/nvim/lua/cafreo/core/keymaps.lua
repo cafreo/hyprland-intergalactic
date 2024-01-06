d37b4dd3502c6996c105d50fa77a43174fc00c93
cafreo <taehyun_uses_tools.se9ou@slmail.me>
2024-01-06 16:31:27 +0100

    added new kaymaps
    - delete selected text with backspace
    - undo and redo via ctrl-z/ctrl-shift-z
    - buffer navigation
    
    - tab to auto complete
    - escape to quit auto complete

--- dotconfig/nvim/lua/cafreo/core/keymaps.lua
+++ dotconfig/nvim/lua/cafreo/core/keymaps.lua
@@ -1,19 +1,47 @@
 local opts = { noremap = true, silent = false }
 
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+local keymap = vim.keymap.set
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+--vim.g.mapleader = " "
+--keymap("n", "<leader>pv", vim.cmd.Ex)
 
 -- Quit neovim / go back to normal mode
-vim.keymap.set("n", "<C-q>", vim.cmd.quit)
-vim.keymap.set("i", "<C-q>", "<Esc>")
+keymap("n", "<C-q>", vim.cmd.quit)
+keymap("i", "<C-q>", "<Esc>")
 
 -- Copy to system keyboard
-vim.keymap.set({"n", "v", "i"}, "<C-c>", '"+y')
-vim.keymap.set({"n", "v", "i"}, "<C-c>", '"+Y')
+keymap({"n", "v", "i"}, "<C-c>", '"+y')
+--keymap({"n", "v", "i"}, "<C-c>", '"+Y')
 
 -- Paste from system keyboard
-vim.keymap.set({"n", "v"}, "<C-v>", '"*P')
-vim.keymap.set("i", "<C-v>", '<C-r>+')
+keymap({"n", "v"}, "<C-v>", '"*P')
+keymap("i", "<C-v>", '<C-r>+')
 
 -- Cut to system keyboard
-vim.keymap.set({"v", "i"}, "<C-x>", '"+x')
+keymap({"v", "i"}, "<C-x>", '"+x')
+
+-- Delete selected Text
+keymap({"v"}, "<Bs>", "d")
+keymap({"n", "v", "i"}, "<S-Del>", "DELETE")
+
+-- Undo and Redo
+keymap({"n"}, "<C-z>", "u")
+keymap({"i"}, "<C-z>", "<C-o>u")
+keymap({"n"}, "<C-S-z>", "<C-r>")
+keymap({"i"}, "<C-S-z>", "<C-o><C-r>")
+
+-- Navigate buffers
+keymap("n", "<S-PgUp>", ":bnext<CR>")
+keymap("n", "<S-PgDown>", ":bprevious<CR>")
+
+-- Move text up and down
+keymap("v", "<S-Up>", ":m .+1<CR>==")
+keymap("v", "<S-Down>", ":m .-2<CR>==")
--- dotconfig/nvim/lua/cafreo/plugins/cmp.lua
+++ dotconfig/nvim/lua/cafreo/plugins/cmp.lua
@@ -10,7 +10,7 @@ return {
     	"onsails/lspkind.nvim", -- vs-code like pictograms
   	},
 	config = function()
-local cmp = require("cmp")
+    local cmp = require("cmp")
 
     local luasnip = require("luasnip")
 
@@ -29,13 +29,13 @@ local cmp = require("cmp")
         end,
       },
       mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        --["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+        --["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+        --["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        --["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Esc>"] = cmp.mapping.abort(), -- close completion window
+	["<Tab>"] = cmp.mapping.confirm({ select = true }),
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
@@ -43,13 +43,23 @@ local cmp = require("cmp")
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
+	{ name = "cmdline"},
+	{ name = "nerdfont"},
+	{ name = "html-css"},
       }),
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
         format = lspkind.cmp_format({
           maxwidth = 50,
           ellipsis_char = "...",
-        }),
+	
+	format = function(entry, vim_item)
+	  if entry.source.name == "html-css" then
+		vim_item.menu = entry.completion_item.menu
+		end
+		return vim_item
+	end
+	}),
       },
     })
   end,
