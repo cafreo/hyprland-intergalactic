0a62157b4a03f24fd915a5be1a72858df54199ad
cafreo <taehyun_uses_tools.se9ou@slmail.me>
2024-01-06 15:55:04 +0100

    some new settings
    disabled wrap and linebreak, enabled highlight search, set clipboard for copy/paste

--- dotconfig/nvim/lua/cafreo/core/settings.lua
+++ dotconfig/nvim/lua/cafreo/core/settings.lua
@@ -2,7 +2,7 @@ vim.cmd('colorscheme default')
 
 syntax = true
 
-vim.opt.nu = true
+vim.opt.number = true
 
 vim.opt.incsearch = true
 
@@ -14,3 +14,20 @@ vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.conceallevel = 0
+vim.opt.hlsearch = true  
+vim.opt.ignorecase = true  
+vim.opt.mouse = "a" 
+vim.opt.showtabline = 2  
+vim.opt.splitbelow = true
+vim.opt.splitright = true 
+vim.opt.cursorline = true
+vim.opt.relativenumber = false   
+vim.opt.numberwidth = 2  
+
+vim.opt.wrap = false
+vim.opt.linebreak = true
+vim.opt.list = false
