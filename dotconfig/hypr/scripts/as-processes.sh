a56755012d4884f8d1daf30b04d00ad9d7184cb3
cafreo <taehyun_uses_tools.se9ou@slmail.me>
2024-02-02 18:16:27 +0100

    reduced sleep timers

--- dotconfig/hypr/scripts/as-processes.sh
+++ dotconfig/hypr/scripts/as-processes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # wait for the script to start
-sleep 12
+sleep 2
 
 # mute microphone
 pactl set-source-mute @DEFAULT_SOURCE@ 1
@@ -11,7 +11,9 @@ pactl set-sink-port 0 analog-output-lineout
 
 # start mullvad
 mullvad disconnect
+mullvad dns set default --block-ads --block-trackers --block-malware
 mullvad relay set hostname ch-zrh-wg-00$(echo $(( $RANDOM % 4 + 1 )))
+#mullvad relay set hostname no-osl-wg-00$(echo $(( $RANDOM % 6 + 1 )))
 mullvad connect
-sleep 6
+sleep 2
 dunstify -t 3000 -a "mlvd" -b "Mullvad" "<b>$(mullvad status)</b>" -u low -i ~/Scripts/img/wifi.svg
