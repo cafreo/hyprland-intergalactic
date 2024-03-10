0da06cc3a16ee870f8be8e5b738df0e4d67d4610
cafreo <taehyun_uses_tools.se9ou@slmail.me>
2024-03-10 13:58:27 +0100

    icons dependent on volume level
    - made it easier to use without dunst

--- dotconfig/notify-send/scripts/notify_volumectl.sh
+++ dotconfig/notify-send/scripts/notify_volumectl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # changeVolume
- 
+
 # Arbitrary but unique message tag
 msgTag="volumectl"
  
@@ -12,27 +12,28 @@ fi
  
  
 # Query amixer for the current volume and whether or not the speaker is muted
-volume="$(pactl list sinks | grep -A 7 "$(pactl info | grep 'Default Sink' | cut -d' ' -f 3)" | grep Volume | awk '{print $5}')"
+volume="$(pactl list sinks | grep -A 7 "$(pactl info | grep 'Default Sink' | cut -d' ' -f 3)" | grep Volume | awk '{print $5}' | sed 's/%//')"
 mute="$(pactl list sinks | grep -A 7 "$(pactl info | grep 'Default Sink' | cut -d' ' -f 3)" | grep Mute | awk '{print $2}')"
-if [[ $volume == 0 || "$mute" == "yes" ]]; then
-    # Show the sound muted notification
-    notify-send -t 1000 -a "volumectl" "Volume muted" -u low -i ~/.config/icons/mute.svg -h string:x-dunst-stack-tag:$msgTag "0" 
 
-elif [[ $volume < 75 && $volume > 25 ]]; then
- Show the volume notification
-    notify-send -t 1000 -a "volumectl" "Volume: $volume" -u low -i ~/.config/icons/volume_high.svg -h string:x-dunst-stack-tag:$msgTag -h int:value:"$volume"
+if [[ $volume -eq 0 || "$mute" == "yes" ]]; then  
+# Show the sound muted notification  
+notify-send -t 1000 -a "volumectl" "Volume muted" -u low -i ~/.config/icons/mute.svg -r 2215
 
-elif [[ $volume < 25 && $volume > 0 ]]; then
- Show the volume notification
-    notify-send -t 1000 -a "volumectl" "Volume: $volume" -u low -i ~/.config/icons/volume_high.svg -h string:x-dunst-stack-tag:$msgTag -h int:value:"$volume"
+elif [[ $volume -le 25 && $volume -gt 0 ]]; then  
+# Show the volume notification  
+notify-send -t 1000 -a "volumectl" "Volume: $volume%" -u low -i ~/.config/icons/volume_low.svg -h int:value:"$volume" -r 2215
 
-elif [[ $volume > 75 ]]; then
-    # Show the volume notification
-    notify-send -t 1000 -a "volumectl" "Volume: $volume" -u low -i ~/.config/icons/volume_high.svg -h string:x-dunst-stack-tag:$msgTag -h int:value:"$volume"
+elif [[ $volume -lt 75 && $volume -gt 25 ]]; then  
+# Show the volume notification  
+notify-send -t 1000 -a "volumectl" "Volume: $volume%" -u low -i ~/.config/icons/volume_medium.svg -h int:value:"$volume" -r 2215
 
-else
-    # Show the volume notification
-    notify-send -t 1000 -a "volumectl" "Volume: $volume" -u low -i ~/.config/icons/volume_high.svg -h string:x-dunst-stack-tag:$msgTag -h int:value:"$volume"
+elif [[ $volume -ge 75% ]]; then  
+# Show the volume notification  
+notify-send -t 1000 -a "volumectl" "Volume: $volume%" -u low -i ~/.config/icons/volume_high.svg -h int:value:"$volume" -r 2215
+
+else  
+# Show the volume notification  
+notify-send -t 1000 -a "volumectl" "Volume: $volume%" -u low -i ~/.config/icons/volume_high.svg -h int:value:"$volume" -r 2215
+
 fi
- 
  
