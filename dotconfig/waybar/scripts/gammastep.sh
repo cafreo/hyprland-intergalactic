15b5a4a2accb943b9f7f89b516e40f0ef1485db0
cafreo <taehyun_uses_tools.se9ou@slmail.me>
2024-03-15 00:21:11 +0100

    cleanup files

--- dotconfig/waybar/scripts/gammastep.sh
+++ dotconfig/waybar/scripts/gammastep.sh
@@ -4,10 +4,10 @@ if [[ $1 = "toggle" ]]; then
 	if pgrep -x "gammastep" > /dev/null; then
 		kill $(pgrep -x gammastep)
 		echo "Nightlight is off"
-		dunstify -t 3000 -a "gammastep" -b "Gammastep" "<b>Nightlight is off</b>" -u low -i ~/Scripts/img/sun.svg -r 7113
+		notify-send -t 3000 -a "gammastep" "Gammastep" "<b>Nightlight is off</b>" -u low -i ~/.config/icons/sun.svg -r 7113
 	else
 		gammastep &
-		dunstify -t 3000 -a "gammastep" -b "Gammastep" "<b>Nightlight timer is on</b>" -u low -i ~/Scripts/img/moon.svg -r 7113
+		notify-send -t 3000 -a "gammastep" "Gammastep" "<b>Nightlight timer is on</b>" -u low -i ~/.config/icons/moon.svg -r 7113
 	fi
 fi
 
