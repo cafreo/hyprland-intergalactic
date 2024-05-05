a1bbda2e556eb51ec38d02837488287f7bee175b
cafreo <taehyun_uses_tools.se9ou@slmail.me>
2024-05-05 15:28:42 +0200

    add notify-send to autostart script

--- dotconfig/hypr/scripts/as-processes.sh
+++ dotconfig/hypr/scripts/as-processes.sh
@@ -5,15 +5,17 @@ sleep 2
 
 # mute microphone
 pactl set-source-mute @DEFAULT_SOURCE@ 1
-
 # set default output to speakers
 pactl set-sink-port 0 analog-output-lineout
+
+sleep 2
 
 # start mullvad
 mullvad disconnect
 mullvad dns set default --block-ads --block-trackers --block-malware
-mullvad relay set hostname ch-zrh-wg-00$(echo $(( $RANDOM % 4 + 1 )))
-#mullvad relay set hostname no-osl-wg-00$(echo $(( $RANDOM % 6 + 1 )))
+mullvad relay set location ch-zrh-wg-00$(echo $(( $RANDOM % 4 + 1 )))
+#mullvad relay set location no-osl-wg-00$(echo $(( $RANDOM % 6 + 1 )))
+#mullvad relay set location de-dus-wg-00$(echo $(( $RANDOM % 2 + 1 )))mullvad connect
 mullvad connect
 sleep 2
-dunstify -t 3000 -a "mlvd" -b "Mullvad" "<b>$(mullvad status)</b>" -u low -i ~/.config/icons/wifi.svg
+notify-send -t 3000 -a "mlvd" "Mullvad" "<b>$(mullvad status)</b>" -u low -i ~/.config/icons/wifi.svg
