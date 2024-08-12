012234d3aa500bb98a600026ed674efd899d83df
cafreo <taehyun_uses_tools.se9ou@slmail.me>
2024-08-12 11:38:31 +0200

    set hyprland environment variables
    - set default cursor
    - set default gtk theme
    - set variables so qt and java apps work better in wayland

--- dotconfig/hypr/scripts/xdg-desktop-portal-hyprland-start.sh
+++ dotconfig/hypr/scripts/xdg-desktop-portal-hyprland-start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sleep 1
+sleep 4
 killall xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-kde
 killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal
 systemctl --user start xdg-desktop-portal-hyprland &
-sleep 2
+sleep 4
 systemctl --user start xdg-desktop-portal &
