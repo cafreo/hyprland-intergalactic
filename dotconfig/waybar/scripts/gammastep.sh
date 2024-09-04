ac5b45d6b80e61ef39f51d85f34943e33a0295e9
cafreo <taehyun_uses_tools.se9ou@slmail.me>
2024-09-04 20:00:14 +0200

    code indenting and higher intervals for cpu and network module

--- dotconfig/waybar/scripts/gammastep.sh
+++ dotconfig/waybar/scripts/gammastep.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 if [[ $1 = "toggle" ]]; then
-	if pgrep -x "gammastep" > /dev/null; then
-		kill $(pgrep -x gammastep)
-		echo "Nightlight is off"
-		notify-send -t 3000 -a "gammastep" "Gammastep" "<b>Nightlight is off</b>" -u low -i ~/.config/icons/sun.svg -r 7113
-	else
-		gammastep &
-		notify-send -t 3000 -a "gammastep" "Gammastep" "<b>Nightlight timer is on</b>" -u low -i ~/.config/icons/moon.svg -r 7113
-	fi
+    if pgrep -x "gammastep" > /dev/null; then
+        kill $(pgrep -x gammastep)
+        echo "Nightlight is off"
+        notify-send -t 3000 -a "gammastep" "Gammastep" "<b>Nightlight is off</b>" -u low -i ~/.config/icons/sun.svg -r 7113
+    else
+        gammastep &
+        notify-send -t 3000 -a "gammastep" "Gammastep" "<b>Nightlight timer is on</b>" -u low -i ~/.config/icons/moon.svg -r 7113
+    fi
 fi
 
 if pgrep -x "gammastep" > /dev/null; then
-	echo ""
-	echo "Nightlight timer is on"
+    echo ""
+    echo "Nightlight timer is on"
 else
-	echo ""
-	echo "Nightlight is off"
+    echo ""
+    echo "Nightlight is off"
 fi
