389d76825e65d8e9eb89da59f93054f622bbbf32
cafreo <taehyun_uses_tools.se9ou@slmail.me>
2024-11-11 23:36:58 +0100

    small tweaks

--- dotconfig/hypr/scripts/as-processes.sh
+++ dotconfig/hypr/scripts/as-processes.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 # wait for the script to start
-sleep 4
-
-# mute microphone
-pactl set-source-mute @DEFAULT_SOURCE@ 1
-# set default output to speakers
-pactl set-sink-port 0 analog-output-lineout
-
-sleep 1
+sleep 2
 
 # start mullvad
 mullvad disconnect
@@ -20,3 +13,10 @@ mullvad relay set location ch-zrh-wg-00$(echo $(( $RANDOM % 4 + 1 )))
 mullvad connect
 sleep 2
 notify-send -t 3000 -a "mlvd" "Mullvad" "<b>$(mullvad status | head -n 1)</b>" -u low -i ~/.config/icons/wifi.svg
+
+sleep 2
+
+# mute microphone
+pactl set-source-mute @DEFAULT_SOURCE@ 1
+# set default output to speakers
+pactl set-sink-port 0 analog-output-lineout
