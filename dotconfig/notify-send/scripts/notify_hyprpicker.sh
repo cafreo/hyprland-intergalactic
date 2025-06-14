553fa33965fafd7b781cbb23c3249612268cc9a6
cafreo <taehyun_uses_tools.se9ou@slmail.me>
2025-06-14 09:09:33 +0200

    added nextcloud
    - added nextcloud window rules and autostart
    - rofi-calc can now copy the result to clipboard
    - fixed hyprpicker script

--- dotconfig/notify-send/scripts/notify_hyprpicker.sh
+++ dotconfig/notify-send/scripts/notify_hyprpicker.sh
@@ -12,7 +12,7 @@ img="$cache/hyprpicker.jpg"
 touch $img
 
 if [[ $value =~ ^#.* ]]; then
-    magick -define jpeg:size=80x80 -thumbnail 80x80 xc:"$value" $img
+    magick -define jpeg:size=80x80 xc:"$value" -resize 80x $img
     notify-send -t 3000 -a "hyprpicker" "HEX color copied" "<b>$value</b>" -u low -i "$img"
 else
     exit
