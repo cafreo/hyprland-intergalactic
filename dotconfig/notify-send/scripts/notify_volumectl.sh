004aa231c9c29138c02609c75f617d7299e3d80d
cafreo <taehyun_uses_tools.se9ou@slmail.me>
2026-05-15 15:35:56 +0200

    rewrote whole volumectl notificationscript
    - rewrote whole volumectl notification script to include option to mute speakers or microphone and adjust volume for both of them
    - added fitting icons for notification

--- dotconfig/notify-send/scripts/notify_volumectl.sh
+++ dotconfig/notify-send/scripts/notify_volumectl.sh
@@ -1,39 +1,75 @@
-#!/bin/bash
-# changeVolume
+#!/bin/sh
 
-# Arbitrary but unique message tag
-msgTag="volumectl"
- 
-if [[ "$@" == "toggle" ]]; then
-    pactl set-sink-mute 0 "$@" > /dev/null
-else
-    pactl set-sink-volume 0 "$@" > /dev/null
+if ! command -v notify-send &> /dev/null; then
+    echo "error: notify-send (libnotify) is not installed."
+    exit 1
 fi
- 
- 
-# Query amixer for the current volume and whether or not the speaker is muted
-volume="$(pactl list sinks | grep -A 7 "$(pactl info | grep 'Default Sink' | cut -d' ' -f 3)" | grep Volume | awk '{print $5}' | sed 's/%//')"
-mute="$(pactl list sinks | grep -A 7 "$(pactl info | grep 'Default Sink' | cut -d' ' -f 3)" | grep Mute | awk '{print $2}')"
 
-if [[ $volume -eq 0 || "$mute" == "yes" ]]; then  
-# Show the sound muted notification  
-notify-send -t 1000 -a "volumectl" "Volume muted" -u low -i ~/.config/icons/mute.svg -r 2215
-
-elif [[ $volume -le 25 && $volume -gt 0 ]]; then  
-# Show the volume notification  
-notify-send -t 1000 -a "volumectl" "Volume: $volume%" -u low -i ~/.config/icons/volume_low.svg -h int:value:"$volume" -r 2215
-
-elif [[ $volume -lt 75 && $volume -gt 25 ]]; then  
-# Show the volume notification  
-notify-send -t 1000 -a "volumectl" "Volume: $volume%" -u low -i ~/.config/icons/volume_medium.svg -h int:value:"$volume" -r 2215
-
-elif [[ $volume -ge 75% ]]; then  
-# Show the volume notification  
-notify-send -t 1000 -a "volumectl" "Volume: $volume%" -u low -i ~/.config/icons/volume_high.svg -h int:value:"$volume" -r 2215
-
-else  
-# Show the volume notification  
-notify-send -t 1000 -a "volumectl" "Volume: $volume%" -u low -i ~/.config/icons/volume_high.svg -h int:value:"$volume" -r 2215
-
+if ! command -v pactl &> /dev/null; then
+    echo "error: pactl (pulseaudio) is not installed."
+    exit 1
 fi
- 
+
+if [ $# -ne 2 ]; then
+    echo "usage: {speakers|mic} {toggle|volume}"
+    exit 1
+fi
+
+SINK=$(pactl get-default-sink)
+SOURCE=$(pactl get-default-source)
+
+case "$1" in
+    "speakers")
+        case "$2" in
+            "toggle")
+                pactl set-sink-mute "$SINK" toggle
+                ;;
+            *)
+                pactl set-sink-volume "$SINK" $2
+                ;;
+        esac
+
+        SINK_VOLUME="$(pactl get-sink-volume "$SINK" | grep -oP '[0-9]+%' | head -1 | sed 's/%//g')"
+        SINK_MUTED="$(pactl get-sink-mute "$SINK" | grep -q "yes" && echo "true" || echo "false")"
+
+        if [[ $SINK_VOLUME -eq 0 || "$SINK_MUTED" == "true" ]]; then    
+            notify-send -t 1000 -a "volumectl" "Volume muted" -u low -i ~/.config/icons/voSpeakerMute.svg -r 2215
+        elif [[ $SINK_VOLUME -le 25 && $SINK_VOLUME -gt 0 ]]; then  
+            notify-send -t 1000 -a "volumectl" "Volume: $SINK_VOLUME%" -u low -i ~/.config/icons/voSpeakerLow.svg -h int:value:"$SINK_VOLUME" -r 2215
+        elif [[ $SINK_VOLUME -lt 75 && $SINK_VOLUME -gt 25 ]]; then  
+            notify-send -t 1000 -a "volumectl" "Volume: $SINK_VOLUME%" -u low -i ~/.config/icons/voSpeakerMedium.svg -h int:value:"$SINK_VOLUME" -r 2215
+        elif [[ $SINK_VOLUME -ge 75 ]]; then  
+            notify-send -t 1000 -a "volumectl" "Volume: $SINK_VOLUME%" -u low -i ~/.config/icons/voSpeakerHigh.svg -h int:value:"$SINK_VOLUME" -r 2215
+        else
+            notify-send -t 1000 -a "volumectl" "Volume: $SINK_VOLUME%" -u low -i ~/.config/icons/voSpeakerHigh.svg -h int:value:"$SINK_VOLUME" -r 2215
+        fi
+
+        ;;
+
+    "mic")
+        case "$2" in
+            "toggle")
+                # Toggle mute status of the microphone
+                pactl set-source-mute "$SOURCE" toggle
+                ;;
+            *)
+                pactl set-source-volume "$SOURCE" $2
+                ;;
+        esac
+
+        SOURCE_VOLUME="$(pactl get-source-volume "$SOURCE" | grep -oP '[0-9]+%' | head -1 | sed 's/%//g')"
+        SOURCE_MUTED="$(pactl get-source-mute "$SOURCE" | grep -q "yes" && echo "true" || echo "false")"
+
+        if [[ $SOURCE_VOLUME -eq 0 || "$SOURCE_MUTED" == "true" ]]; then    
+            notify-send -t 1000 -a "volumectl" "Volume muted" -u low -i ~/.config/icons/voMicMute.svg -r 2215
+        else
+            notify-send -t 1000 -a "volumectl" "Volume: $SOURCE_VOLUME%" -u low -i ~/.config/icons/voMic.svg -h int:value:"$SOURCE_VOLUME" -r 2215
+        fi
+
+        ;;
+
+    *)
+        echo "usage: {speakers|mic} {toggle|volume}"
+        exit 1
+        ;;
+esac
